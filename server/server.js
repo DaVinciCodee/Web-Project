8011683b071005f6ac63d2cd0c1ef02574ea3e42
usrlocal <usrlocal@GE22-FE19.ad.univ-angers.fr>
2025-11-18 16:20:05 +0100

    authenification de l'app ok

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use('/api/auth', require('./routes/auth'));
 // app.use('/api/users', require('./routes/users'));
 
 // Démarrage serveur
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
 });
