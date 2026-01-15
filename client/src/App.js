b780bac6812d1feadf9b5b59914a1b0f98616d39
Matlef53 <matlef.53@gmail.com>
2026-01-15 15:21:51 +0100

    modif

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Profile from './pages/Profile/Profile';
 import Navbar from './components/Navbar/Navbar';
